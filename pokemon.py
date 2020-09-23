1efca41a2e0201953bc082c5a4e2e8b94a6c2eae
Vưu Văn Tòng (Eric) <silverwolf@ceh.vn>
2020-09-23 06:59:30 +0000

    Able to control rare Pokemon list

--- pokemon.py
+++ pokemon.py
@@ -4,22 +4,20 @@ from telegrambot import telegrambot
 import json
 import requests
 
-RARE_LIST = ["togetic", "unown", "dragonite", "tyranita", "scyther", "porygon"]
+RARE_LIST = ["togetic", "unown", "dragonite", "tyranita", "scyther", "lapras"]
 def parse_ok_cb(data):
 	detail = data["detail"]
 	pname = detail["name"]
 	region_detector = pname.split(" ")[0]
 	if region_detector == "Alolan":
 		pname = pname.split(" ")[1]
-	if pname.lower() in RARE_LIST or int(detail["L"]) >= 30 or int(detail["cp"]) >= 2500:
+	if pname.lower() in telegrambot.get_rare_poke() or int(detail["L"]) >= 30 or int(detail["cp"]) >= 2500:
 		print(detail)
-		#new_url = "https://pokefree.silverwolfceh.repl.co/revealcoords?url=" + detail["urlcode"]
 		new_url = detail["url"]
 		telegram_fmt = "%s CP%s L%s URL: %s" % (detail["name"], detail["cp"], detail["L"],new_url)
 		telegrambot.sendMessage("@tongvuu", telegram_fmt)
-		#requests.get("https://pokefree.silverwolfceh.repl.co/posttome?message=%s" % telegram_fmt)
 	else:
-		print("Pokemon %s not sastify the condition" % detail.name )
+		print("Pokemon %s not sastify the condition" % detail["name"] )
 
 def parse_err_cb(data):
 	pass
